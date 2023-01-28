23c56e703cd3ce35e4ff950f172b5f51732994cc
Ravi Mogha <121372488+rmogha395@users.noreply.github.com>
2023-01-28 13:20:16 +0530

    Add files via upload

--- SQL Project 1(Mavenmovies).sql
+++ SQL Project 1(Mavenmovies).sql
@@ -2,33 +2,35 @@ show databases;
 use mavenmovies;
 show tables;
 
-#QUERY 1: 
+#QUERY 1:SQL query to count the number of characters except the spaces for each actor. 
+#Return first 10 actors name length along with their name.
+
 SELECT LENGHT(CONCAT(first_name,last_name)) from actor LIMIT 10;
 
 
-#QUERY 2: 
+#QUERY 2: List all Oscar awardees(Actors who received Oscar award) with their full names and length of their names.
 SELECT CONCAT(first_name,' ',last_name) as fullname, LENGTH(CONCAT(first_name,last_name))as length_full_name from
          actor_award where awards IN ('Oscar');
 
 
-#QUERY 3: 
+#QUERY 3: Find the actors who have acted in the film ‘Frost Head’.
 SELECT film.title, actor.first_name, actor.last_name from film
          INNER JOIN film_actor on film.film_id=film_actor.film_id
          INNER JOIN actor on actor.actor_id=film_actor.actor_id WHERE title='Frost Head';
 
 
-#QUERY 4: 
+#QUERY 4: Pull all the films acted by the actor ‘Will Wilson’.
 SELECT film.title, actor.first_name, actor.last_name from film
          INNER JOIN film_actor on film.film_id=film_actor.film_id
          INNER JOIN actor on actor.actor_id=film_actor.actor_id WHERE first_name='Will' AND last_name='Wilson';
 
 
-#QUERY 5: 
+#QUERY 5: Pull all the films which were rented and return in the month of May.
 SELECT film.title, rental.return_date, rental.rental_date from film
          CROSS JOIN rental where return_date like '%-05-%' AND rental_date like '%-05-%';
 
 
-#QUERY 6: 
+#QUERY 6: Pull all the films with ‘Comedy’ category.
 SELECT film.title, category.name from category
          CROSS JOIN film where name='Comedy';
 
